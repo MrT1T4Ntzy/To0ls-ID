a4e33e1d32608da73cb51ec3ed4041d27603291e
MrT1T4Ntzy <91118364+MrT1T4Ntzy@users.noreply.github.com>
2021-10-04 12:08:25 +0700

    Update T00ls-ID.sh

--- T00ls-ID.sh
+++ T00ls-ID.sh
@@ -7,9 +7,11 @@ echo
 echo "--------------------------------------
 echo "Author : Mr.T1T4N
 echo "Team : Mafia Teknologi
+echo "Youtube : T1T4N TZY
 echo -----------------------------------------"
 echo "{!} SILAKAN PILIH SEMUA TOOLSNYA DI BAWAH INI"
 echo "GUNAKAN SECUKUPNYA"
+
    sleep 2
 echo "{1} Hack CCTV [MAIN]"
 echo "{2} Teks Ganas [ON]"
@@ -19,38 +21,34 @@ echo "{99} KELUAR !"
 sleep 1
 read -p "Cepat NGAB Pilih: " pilih
 # data menu 1
-if [[ $pilih = '1' ]]
+if [ $pilih = '1' ]
 then
   git clone https://github.com/MrVirusSpm-07/cchek
   cd cchek
   sh cchek.sh
-fi
 # data menu 2
-if [[ $pilih = '2' ]]
+if [ $pilih = '2' ]
 then
   git clone https://github.com/Mr-X-junior/Virtex
   cd Virtex
   python virtex.py
-fi
 # data menu 3
-if [[ $pilih = '3' ]]
+if [ $pilih = '3' ]
 then
   git clone https://github.com/MrVirusSpm-07/juz-amma
   cd juz-amma
   sh amma.sh
-fi
 # data menu 4
-if [[ $pilih = '4' ]]
+if [ $pilih = '4' ]
 then
   git clone https://github.com/4L13199/LITESCRIPT
   cd LITESCRIPT
   python2 LITESCRIPT.py
 fi
 # data menu 5
-if [[ $pilih = '99' ]]
+if [ $pilih = '99' ] 
 then
-   figlet Terima | lolcat
-   figlet Kasih | lolcat
-   date | lolcat
+   echo "TERIMAKASIH TELAH MENGGUNAKAN TOOLS INI YA SOBBB"
+   echo "DAN SUBCRIBE CHANNEL : T1T4N TZY"
    exit
-done
+

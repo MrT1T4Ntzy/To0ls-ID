4256b23fc0040b9085b7671f27ef01051943165e
MrT1T4Ntzy <91118364+MrT1T4Ntzy@users.noreply.github.com>
2021-10-06 09:52:00 +0700

    Update Ng3t00lS.sh

--- Ng3t00lS.sh
+++ Ng3t00lS.sh
@@ -8,12 +8,12 @@ sleep 2
 echo "SELAMAT DATANG DI TOOLS INI YG MENYAJIKAN BERAGAM BANYAK TOOLS WALAUPUN SEDIKIT"
 echo "DAN GUNAKAN DEGAN BIJAK"
 
-echo "\x1b[32;1m[1] Hack CCTV"
-echo "\x1b[32;1m[2] Bikin VIRTEX"
-echo "\x1b[32;1m[3] Kirim Virus"
+echo '[1] Hack CCTV"
+echo "[2] Bikin VIRTEX"
+echo "[3] Kirim Virus"
 echo
-echo "\x1b[31;1m[99] Keluar Aja Yukk"
-read "Cepat Pilih NGAB :"
+echo "[99] Keluar Aja Yukk"
+read "Cepat Pilih:"
 if [ $pilih = '1' || [ $pilih = '01' ]
 then
 git clone https://github.com/MrVirusSpm-07/cchek
@@ -35,4 +35,3 @@ figlet TERIMA | lolcat
 figlet KASIH | lolcat
 date | lolcat
 exit
-done
